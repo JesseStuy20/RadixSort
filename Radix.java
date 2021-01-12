656a0ac9f29953920323d41572d4db39cb00e9d2
JesseStuy20 <jxie20@stuy.edu>
2021-01-12 15:23:08 -0500

    Fixed file name

---
+++ Radix.java
@@ -0,0 +1,26 @@
+public class Radix {
+
+  public static int nth(int n, int col) {
+    int divisor = (int) Math.pow(10,col+1) ;
+    int returnValue = n % divisor ;
+    return (returnValue / (divisor/10)) ;
+  }
+
+  public static int length(int n) {
+    int returnValue = 1 ;
+    for (int i=n; i>10; i=i/10) {
+      returnValue++ ;
+    }
+    return returnValue ;
+  }
+
+  public static void merge(MyLinkedList original, MyLinkedList[] buckets) {
+    MyLinkedList a = new MyLinkedList() ;
+    a = buckets[0] ;
+    for (int i=1; i<10; i++) {
+      a.extend(buckets[i]) ;
+    }
+    original.extend(a) ;
+  }
+
+}
