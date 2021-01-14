6e44f3d2616e999e341b1ab542c4f3bf6336a8db
JesseStuy20 <jxie20@stuy.edu>
2021-01-14 13:26:14 -0500

    Added issues with time

--- Radix.java
+++ Radix.java
@@ -22,20 +22,20 @@ public class Radix {
   public static void radixSortSimple(SortableLinkedList data) {
     int largestNum = Math.abs(data.get(0)) ;
     SortableLinkedList[] a = new SortableLinkedList[10] ;
+    int dataSize = data.size() ;
     for (int i=0; i<10; i++) {
       a[i] = new SortableLinkedList() ;
     }
     for (int i=0; i<length(largestNum); i++) { //digits
-      for (int j=0; j<data.size(); j++) { //node
-        int temp = data.get(j) ;
-        if (Math.abs(temp) > largestNum) largestNum = temp ;
+      for (int j=0; j<dataSize; j++) { //node
+        int temp = data.get(0) ;
+        if ((i == 0) && (Math.abs(temp) > largestNum)) largestNum = temp ;
         int b = nth(temp,i) ;
         a[b].add(temp) ;
-      }
-      merge(data,a) ;
-      for (int j=0; j<data.size();j++) {
         data.remove(0) ;
       }
+      merge(data,a) ;
+
     }
   }
 
