7f11f12e604a1766a2ea4c4f969868a2c9f4d478
JesseStuy20 <jxie20@stuy.edu>
2021-01-15 15:22:38 -0500

    Updated RadixSort method

--- Radix.java
+++ Radix.java
@@ -35,10 +35,9 @@ public class Radix {
         data.remove(0) ;
       }
       merge(data,a) ;
-
     }
   }
-
+/*
   public static void radixSort(SortableLinkedList data) {
     SortableLinkedList positive = new SortableLinkedList() ;
     SortableLinkedList negative = new SortableLinkedList() ;
@@ -59,26 +58,25 @@ public class Radix {
     }
     data.extend(positive) ;
   }
+*/
 
-/*
   public static void radixSort(SortableLinkedList data) {
     int largestNum = Math.abs(data.get(0)) ;
     SortableLinkedList[] a = new SortableLinkedList[20] ;
+    int dataSize = data.size() ;
     for (int i=0; i<20; i++) {
       a[i] = new SortableLinkedList() ;
     }
     for (int i=0; i<length(largestNum); i++) { //digits
-      for (int j=0; j<data.size(); j++) { //node
-        int temp = data.get(j) ;
-        if (Math.abs(temp) > largestNum) largestNum = temp ;
+      for (int j=0; j<dataSize; j++) { //node
+        int temp = data.get(0) ;
+        if ((i == 0) && (Math.abs(temp) > largestNum)) largestNum = temp ;
         int b = nth(temp,i) + 10 ;
         a[b].add(temp) ;
-      }
-      merge(data,a) ;
-      for (int j=0; j<data.size();j++) {
         data.remove(0) ;
       }
+      merge(data,a) ;
     }
   }
-*/
+
 }
